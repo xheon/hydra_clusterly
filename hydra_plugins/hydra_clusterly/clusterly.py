0bfaebb1b25e179c5f15bdef48da0b32650a9588
Manuel Dahnert <manuel.dahnert@gmail.com>
2022-02-24 15:49:36 +0100

    Add continuous display of job output for single-runs.

--- hydra_plugins/hydra_clusterly/clusterly.py
+++ hydra_plugins/hydra_clusterly/clusterly.py
@@ -1,36 +1,32 @@
+import datetime
 import inspect
+import logging
 import os
-import shutil
 import time
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, Sequence, Any, List, Dict, Tuple
 
 import sysrsync
-
-import logging
-from pathlib import Path
-from typing import Optional, Sequence, Any, List, Dict
-
-from hydra.types import HydraContext
+import tailhead
 from hydra.core.config_store import ConfigStore
 from hydra.core.singleton import Singleton
+from hydra.core.utils import JobReturn, setup_globals
 from hydra.core.utils import (
-    JobReturn,
-    configure_log,
     filter_overrides,
     run_job,
-    setup_globals,
 )
-from hydra.plugins.launcher import Launcher
+from hydra.types import HydraContext
 from hydra.types import TaskFunction
 from hydra_plugins.hydra_submitit_launcher.config import BaseQueueConf, SlurmQueueConf
 from hydra_plugins.hydra_submitit_launcher.submitit_launcher import SlurmLauncher
 from omegaconf import DictConfig, open_dict, OmegaConf
-
-from hydra.core.utils import JobReturn, setup_globals, configure_log
+from submitit import Executor, Job
 
 log = logging.getLogger(__name__)
 
 
+# Extend Slurm configuration with additional fields
 @dataclass
 class ClusterlyConf(SlurmQueueConf):
     _target_: str = (
@@ -41,7 +37,11 @@ class ClusterlyConf(SlurmQueueConf):
     code_ignores: Optional[List[str]] = None
     code_ignore_file: Optional[str] = None
 
+    print_output: bool = False
+    wait_for_completion: bool = True
 
+
+# Register as hydra launcher
 ConfigStore.instance().store(
     group="hydra/launcher", name="clusterly", node=ClusterlyConf
 )
@@ -66,13 +66,12 @@ class Clusterly(SlurmLauncher):
     def copy_code(self, task_function):
         # Determine common path prefix.
         task_function_path = Path(inspect.getsourcefile(task_function))  # from task function
-        config_path = Path(self.config.hydra.runtime.config_sources[1].path)   # from config
+        config_path = Path(self.config.hydra.runtime.config_sources[1].path)  # from config
         common_prefix = Path(os.path.commonpath([task_function_path, config_path]))
         log.info(f"Clusterly: Determined source code path: {common_prefix}")
 
         self.code_path.mkdir(exist_ok=True, parents=True)
         log.info(f"Clusterly: Determined destination code path: {self.code_path}")
-
         copy_info = f"Clusterly: Copy source files"
 
         # Read ignores from file.
@@ -151,7 +150,10 @@ class Clusterly(SlurmLauncher):
                 if x in specific_init_keys
             }
         )
-        init_keys = specific_init_keys | {"submitit_folder", "code_path", "code_ignores", "code_ignore_file"}
+
+        init_keys = specific_init_keys | {"submitit_folder", "code_path", "code_ignores", "code_ignore_file",
+                                          "print_output", "wait_for_completion"}
+
         executor = submitit.AutoExecutor(cluster=self._EXECUTOR, **init_params)
 
         # specify resources/parameters
@@ -161,6 +163,7 @@ class Clusterly(SlurmLauncher):
             for x, y in params.items()
             if x not in init_keys
         }
+        params["stderr_to_stdout"] = True
         executor.update_parameters(**params)
 
         # distinguish between single and multi run
@@ -182,12 +185,125 @@ class Clusterly(SlurmLauncher):
             Singleton.get_state(),
         ]
 
-        job = executor.submit(self.submit_single_job, *job_params)
-        log.info(f"Clusterly: Submitted single job as {job.job_id}")
+        job = executor.submit(self.submit_job, *job_params)
+
+        log.info(f"Clusterly: Submitted single job as {job.job_id} [{job.state}]")
+
+        if not self.params["wait_for_completion"]:
+            log.info(f"Clusterly: Job as {job.job_id} is still running.")
+            # return [JobReturn(_return_value="uncompleted")]
+            return []
+
+        last_state = job.state
+        last_state_change = time.perf_counter()
+        last_position_stdout = 0
+
+        if self.params["print_output"]:
+            printer = tailhead.follow_path(job.paths.stdout)
+
+        while not job.done():
+            try:
+                if self.params["print_output"]:
+                    next_line = next(printer)
+                    if next_line is not None and next_line != "":
+                        lines = next_line.split("\r")
+
+                        if len(lines) == 1:
+                            print(next_line)
+                        else:
+                            for line in lines:
+                                if line != "":
+                                    print(line, end="\r")
+
+                # Display changes in job status
+                state_has_changed, new_state, last_state_change, delta_seconds = self.check_state_change(job,
+                                                                                                         last_state,
+                                                                                                         last_state_change)
+                if state_has_changed:
+                    self.log_state_change(job.job_id, last_state, new_state, delta_seconds)
+                    last_state = new_state
+
+                time.sleep(0.2)
+            except KeyboardInterrupt:
+                log.info(f"Output stopped, job is still running at {job.job_id}")
+
+                while True:
+                    confirm = input("[r] Resume, [c] Cancel job, [q] Exit: ")
+                    if confirm == "r":
+                        log.info(f"Resume output...")
+                        break
+                    elif confirm == "c":
+                        log.info(f"Cancelling job {job.job_id}...")
+                        job.cancel()
+                        log.info(f"Job {job.job_id} cancelled.")
+                        return []
+                    elif confirm == "q":
+                        log.info(f"Exited, job is still running at {job.job_id}")
+                        return []
+
+        if self.params["print_output"]:
+            # self.print_stdout(job, last_position_stdout)
+            for next_line in printer:
+                if next_line == "":
+                    break
+
+                if next_line is not None and next_line != "":
+                    lines = next_line.split("\r")
+
+                    if len(lines) == 1:
+                        print(next_line)
+                    else:
+                        for line in lines[:-1]:
+                            if line != "":
+                                print(line, end="\r")
+                        print(lines[-1])
+        log.info(f"Clusterly: Job {job.job_id} done")
+        new_state = job.watcher.get_state(job.job_id, mode="force")
+        self.log_state_change(job.job_id, last_state, new_state, time.perf_counter() - last_state_change)
 
         return [job.result()]
 
-    def launch_multi_run(self, executor, job_overrides, initial_job_idx):
+    @staticmethod
+    def print_stdout(job: Job, last_position_stdout: int) -> int:
+        stdout_path = job.paths.stdout
+        if stdout_path.exists():
+            with stdout_path.open(encoding='utf-8') as stdout:
+                stdout.seek(last_position_stdout)
+
+                stdout_output = ""
+                try:
+                    stdout_output = stdout.readline()
+                except UnicodeDecodeError:
+                    print("Ignoring DecodeError")
+
+                if stdout_output.strip():
+                    lines_read = len(stdout_output)
+                    last_position_stdout += lines_read
+
+                    if lines_read > 0:
+                        print(stdout_output, end="")
+
+        return last_position_stdout
+
+    @staticmethod
+    def log_state_change(job_id: str, last_state: str, new_state: str, delta_seconds: float) -> None:
+        delta_time = datetime.timedelta(seconds=round(delta_seconds, 0))
+        log.info(f"Clusterly: Job {job_id} changed from [{last_state}] to [{new_state}] after {delta_time}")
+
+    @staticmethod
+    def check_state_change(job: Job, last_state: str, last_state_change: float) -> Tuple[bool, str, float, float]:
+        has_changed = False
+        delta_time_seconds = 0
+        if job.state != last_state:
+            delta_time_seconds = time.perf_counter() - last_state_change
+            last_state = job.state
+            last_state_change = time.perf_counter()
+            has_changed = True
+
+        return has_changed, last_state, last_state_change, delta_time_seconds
+
+    def launch_multi_run(self, executor: Executor, job_overrides: Sequence[Sequence[str]],
+                         initial_job_idx: int) -> Sequence[JobReturn]:
         log.info(f"Clusterly: Use '{self._EXECUTOR}', sweep output dir : {self.config.hydra.sweep.dir}")
 
         sweep_dir = Path(str(self.config.hydra.sweep.dir))
